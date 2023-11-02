71deb067628ae5d76e66d155fc423893d1932f5a
wyx-m <86351093+wyx-m@users.noreply.github.com>
2023-11-02 15:50:44 -0400

    Add files via upload

---
+++ src/findPivot.java
@@ -0,0 +1,9 @@
+// Determine the pivot points
+public class findPivot {
+    public void findPivot(Robot eachCircle[]){
+        //condition1:
+        //condition2:
+        //condition3:
+
+    }
+}
